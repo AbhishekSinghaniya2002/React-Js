5b9509474a2aa5f9100f4b50d0e3d057a7cfaa72
Abhishek Singh <abhisheksingh02431@gmail.com>
2024-09-21 11:06:02 +0530

    vitereact

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Chai from "./chai"
+function App() {
+ 
+
+  return (
+    <>
+   <Chai/>
+   <h1> I am fine</h1>
+   
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,11 @@
+import App from "./App"
+
+function Chai(){
+    return(
+        <>
+        <h2>Hello world</h2>
+        <p> Good Programmer </p>
+        </>
+    )
+}
+export default Chai
