f3826d2f380e45731bbaea337cafcdab4d8fdf87
ABHISHEK SINGH <102066156+AbhishekSinghaniya2002@users.noreply.github.com>
2024-10-05 07:54:27 +0530

    Update todoSlice.js

--- 11reduxToolkitTodo/src/features/todo/todoSlice.js
+++ 11reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -37,6 +37,33 @@ export const todoSlice = createSlice({
         removeTodo: (state, action) => {
             state.todos = state.todos.filter((todo) => todo.id !== action.payload )
         },
+
+         updateTodo: (state, action) => {
+            state.todos = state.todos.map((todo) =>
+                todo.id === action.payload.id ? { ...todo, text: action.payload.text } : todo
+            );
+        },
+        
+        // other method 
+                // updateTodo: (state, action) => {
+                //     const { id, text } = action.payload;
+                //     const todo = state.todos.find(todo => todo.id === id);
+                //     if (todo) {
+                //         todo.text = text;
+                //     }
+                // }
+
+                // updatetodo: (state, action)=> {
+
+                //     const id = action.payload.id;
+                //     const text = action.payload.text;
+                //     state.todos = state.todos.map((todo) => {
+                //         if (todo.id === id) {
+                //           todo.text = text;
+                //         }
+                //         return todo;
+                //     });
+
     }
 })
 
